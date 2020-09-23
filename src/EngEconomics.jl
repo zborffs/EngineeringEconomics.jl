7773b9ac972278385d7bb33d2ca613ce2d81edcf
Zach Bortoff <zbortoff@gmail.com>
2020-09-23 16:25:55 -0400

    initial commit

--- src/EngEconomics.jl
+++ src/EngEconomics.jl
@@ -1,5 +1,24 @@
 module EngEconomics
 
-# Write your package code here.
+using Dates
+
+export simpleInterest, findFractionalPeriod
+
+# Simple Interest (F = P(1 + i x n))
+# F = Future amount
+# P = Principal amount
+# i = simple interest rate
+# n = period of time
+function simpleInterest(P::Number, i::Number, n::Number)::Number
+	F = P * (1 + i * n)
+end
+
+# Find Fraction Period (360 days a year, 12 months, 60 days)
+function findFractionalPeriod(date1::Date, date2::Date)::Number
+	monthDays = 30 * (Dates.month(date2) - Dates.month(date1))
+	d = Dates.day(date2) - Dates.day(date1) + 1
+	return (monthDays + d) / 360
+end
+
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,15 @@
 using EngEconomics
 using Test
+using Dates
 
-@testset "EngEconomics.jl" begin
-    # Write your tests here.
+@testset "Simple Interest" begin
+	@test simpleInterest(4500, 0.08, 1) == 4860
+	@test simpleInterest(4500, 0.08, 4) == 5940
+	@test isapprox(simpleInterest(1000, 0.08, 80/360), (1000 + 17.78); atol=0.01)
+end
+
+@testset "Fractional Payment Period" begin
+	apr1 = Date(Dates.Month(4), Dates.Day(1))
+	jun20 = Date(Dates.Month(6), Dates.Day(20))
+	@test findFractionalPeriod2(apr1, jun20) == 80/360
 end
